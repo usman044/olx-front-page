ddeb006a4735165bdcdb2bcc3948cd43806f59ab
usman ali <uali75616@gmail.com>
2022-10-01 16:49:15 -0700

    first commit

---
+++ public/olx.js
@@ -0,0 +1,74 @@
+// alert("how may i help you")
+// console.log("hello world")
+// var world = prompt("whats your age")
+// alert(world)
+
+
+// var num1 = prompt("put number-1")
+// var num2 = prompt("put number-2")
+// alert(Number (num1) + Number (num2))
+
+var username=prompt("whats your name ?")
+alert("Wellcome to olx "+username);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
